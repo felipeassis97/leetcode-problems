7f11717bf6211c5d9ad3ffedf283e8c1ef9835e5
felipeassis97 <felipeassis97@gmail.com>
2024-09-05 22:55:20 +1000

    Two numbers problem 1 Leetcode

---
+++ day_1/two_sum.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val numMap = mutableMapOf<Int, Int>()
+
+        for (i in nums.indices) {
+            val complement = target - nums[i]
+
+            if (numMap.containsKey(complement)) {
+                return intArrayOf(numMap[complement]!!, i)
+            }
+            numMap[nums[i]] = i
+        }
+
+        throw IllegalArgumentException("No two sum solution")
+    }
+}
